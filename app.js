9a83fdebdf444885e2d0a18c79935544738ffb30
KJ <zkjj1504@gmail.com>
2023-10-30 13:18:13 +0000

    updated files

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
-let app = express();
+const app = express();
 const path = require("path");
+const mysql = require("mysql2");
 
 //middleware
 app.use(express.static(path.join(__dirname, './public')));
 app.set('view engine', 'ejs');
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'kjarosz02',
+        port: '3306'
+    }
+);
+
+connection.connect((err) => {
+
+    if (err) return console.log(err.message);
+
+});
+
 
 //routes
 app.get("/", (req, res) => {
